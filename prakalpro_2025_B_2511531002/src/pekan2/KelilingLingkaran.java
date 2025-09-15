f2729ee6b4cda08afac26f48a7cf3e6c6c9a7ab1
user <user@DESKTOP-RDOSSSB>
2025-09-15 11:35:07 +0700

    praktikum pekan 2

--- prakalpro_2025_B_2511531002/src/pekan2/KelilingLingkaran.java
+++ prakalpro_2025_B_2511531002/src/pekan2/KelilingLingkaran.java
@@ -1,5 +1,10 @@
 package pekan2;
 
 public class KelilingLingkaran {
-
+	public static void main(String[] args) {
+		final double PI = 3.14;
+		double radius = 30;
+			System.out.println("Keliling = " + 2 * PI * radius);
+	}
+	
 }
