8f2bb2f86b537a45f854ba7ec676a35375ba05a9
user <user@DESKTOP-RDOSSSB.bbrouter>
2025-09-15 17:23:51 +0700

    tugas alpro pekan 2

--- prakalpro_2025_B_2511531002/src/pekan2/tugasAlproPekan2.java
+++ prakalpro_2025_B_2511531002/src/pekan2/tugasAlproPekan2.java
@@ -2,11 +2,11 @@ package pekan2;
 
 import java.util.Scanner;
 
-public class DataMahasiswa {
+public class tugasAlproPekan2 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		
-		// Input data
+		//Input data
 		System.out.print("Masukkan NIM Anda: ");
 		String nim = input.nextLine();
 		
@@ -14,21 +14,21 @@ public class DataMahasiswa {
 		String nama = input.nextLine();
 		
 		System.out.print("Masukkan Umur Anda: ");
-		int umur = input.nextInt();	
+		int umur = input.nextInt();
 		
-		// Konversi umur menjadi String
+		//Konversi umur menjadi String
 		String umurString = String.valueOf(umur);
 		
-		// Konversi NIM menjadi Long lalu tambahkan dengan umur
+		//Konversi NIM menjadi Long, lalu tambahkan dengan Umur
 		long nimLong = Long.parseLong(nim);
 		long hasilPenjumlahan = nimLong + umur;
 		
-		// Output
-		System.out.println("\n=== Data Mahasiswa ===\n");	
+		//Output
+		System.out.println("\n=== Data Mahasiswa ===\n");
 		System.out.println("NIM: " + nim);
 		System.out.println("Nama: " + nama);
 		System.out.println("Umur: " + umur + " Tahun");
-
+		
 		System.out.println("\nUmur (String): " + umurString);
 		System.out.println("NIM + Umur: " + hasilPenjumlahan);
 		
