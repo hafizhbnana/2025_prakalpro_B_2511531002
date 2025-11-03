34f2be1b25e12832165fcb2d6b5ed351145b9479
hafizhbnana <hafizhhabibullahbeniska@gmail.com>
2025-11-03 11:35:31 +0700

    Add files via upload

---
+++ prakalpro_2025_B_2511531002/src/pekan6_2511531002/perulanganWhile1_2511531002.java
@@ -0,0 +1,25 @@
+package pekan6_2511531002;
+
+import java.util.Scanner;
+
+public class perulanganWhile1_2511531002 {
+	public static void main(String[] args) {
+		int counter=0;
+		String jawab;
+		boolean running = true;
+		
+		Scanner scan = new Scanner (System.in);
+		while (running) {
+			counter++;
+			System.out.println("Jumlah = "+ counter);
+			System.out.print("Apakah lanjut (ya/tidak?)");
+			jawab = scan.nextLine();
+			
+			if (jawab.equalsIgnoreCase("tidak")) {
+				running = false;
+			}
+		}
+		System.out.println("Anda sudah melakukan perulangan sebanyak "+counter+" kali");
+		scan.close();
+	}
+}
