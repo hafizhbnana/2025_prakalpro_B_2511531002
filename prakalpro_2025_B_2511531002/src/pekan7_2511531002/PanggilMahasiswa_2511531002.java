cc667309ae2eeca378954e07d4e15c6819e69039
hafizhbnana <hafizhhabibullahbeniska@gmail.com>
2025-11-10 11:38:06 +0700

    Add files via upload

---
+++ prakalpro_2025_B_2511531002/src/pekan7_2511531002/PanggilMahasiswa_2511531002.java
@@ -0,0 +1,12 @@
+package pekan7_2511531002;
+
+public class PanggilMahasiswa_2511531002 {
+	public static void main(String[] args) {
+		Mahasiswa_2511531002 a = new Mahasiswa_2511531002();
+		a.setNim(23532);
+		a.setNama("Hafizh");
+		System.out.println(a.getNim());
+		System.out.println(a.getNama());
+		a.Cetak();
+	}
+}
