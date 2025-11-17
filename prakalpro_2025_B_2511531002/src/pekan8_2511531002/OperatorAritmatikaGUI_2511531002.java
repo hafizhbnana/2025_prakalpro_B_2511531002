05e957c4b205796192a3293af1e7bde33ca20921
hafizhbnana <hafizhhabibullahbeniska@gmail.com>
2025-11-17 11:53:23 +0700

    Add files via upload

---
+++ prakalpro_2025_B_2511531002/src/pekan8_2511531002/OperatorAritmatikaGUI_2511531002.java
@@ -0,0 +1,157 @@
+package pekan8_2511531002;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class OperatorAritmatikaGUI_2511531002 extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JTextField txtBil1;
+	private JTextField txtBil2;
+	private JTextField txtHasil;
+	
+	
+	private void pesanPeringatan(String pesan) {
+		JOptionPane.showMessageDialog(this, pesan,"Peringatan", JOptionPane.WARNING_MESSAGE);
+	}
+	private void pesanError(String pesan) {
+		JOptionPane.showMessageDialog(this, pesan,"Kesalahan", JOptionPane.ERROR_MESSAGE);
+	}
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					OperatorAritmatikaGUI_2511531002 frame = new OperatorAritmatikaGUI_2511531002();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public OperatorAritmatikaGUI_2511531002() {
+		setTitle("OPERATOR ARITMATIKA");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 321, 295);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("OPERATOR ARITMATIKA");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setFont(new Font("Formula1 Display Bold", Font.PLAIN, 12));
+		lblNewLabel.setBounds(38, 32, 213, 31);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Bilangan 1");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblNewLabel_1.setBounds(10, 73, 78, 31);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_1_1 = new JLabel("Bilangan 2");
+		lblNewLabel_1_1.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblNewLabel_1_1.setBounds(10, 99, 78, 31);
+		contentPane.add(lblNewLabel_1_1);
+		
+		JLabel lblNewLabel_1_1_1 = new JLabel("Operator");
+		lblNewLabel_1_1_1.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblNewLabel_1_1_1.setBounds(10, 126, 78, 31);
+		contentPane.add(lblNewLabel_1_1_1);
+		
+		JLabel lblNewLabel_1_1_1_1 = new JLabel("Hasil");
+		lblNewLabel_1_1_1_1.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblNewLabel_1_1_1_1.setBounds(10, 167, 78, 31);
+		contentPane.add(lblNewLabel_1_1_1_1);
+		
+		txtBil1 = new JTextField();
+		txtBil1.setHorizontalAlignment(SwingConstants.CENTER);
+		txtBil1.setBounds(98, 80, 56, 19);
+		contentPane.add(txtBil1);
+		txtBil1.setColumns(10);
+		
+		txtBil2 = new JTextField();
+		txtBil2.setHorizontalAlignment(SwingConstants.CENTER);
+		txtBil2.setColumns(10);
+		txtBil2.setBounds(98, 106, 56, 19);
+		contentPane.add(txtBil2);
+		
+		JComboBox cbOperator = new JComboBox();
+		cbOperator.setModel(new DefaultComboBoxModel(new String[] {"+", "-", "*", "/", "%"}));
+		cbOperator.setBounds(98, 132, 41, 21);
+		contentPane.add(cbOperator);
+		
+		txtHasil = new JTextField();
+		txtHasil.setEditable(false);
+		txtHasil.setHorizontalAlignment(SwingConstants.CENTER);
+		txtHasil.setColumns(10);
+		txtHasil.setBounds(98, 174, 56, 19);
+		contentPane.add(txtHasil);
+		
+		JButton btnNewButton = new JButton("Proses");
+		btnNewButton.addActionListener(new ActionListener() {
+			int hasil;
+			public void actionPerformed(ActionEvent e) {
+				if(txtBil1.getText().trim().isEmpty()) {
+					pesanPeringatan("Bilangan 1 harus diisi");
+				} else if (txtBil2.getText().trim().isEmpty()) {
+					pesanPeringatan("Bilangan 2 harus diisi");
+				} else
+				{
+					try {
+						int a = Integer.parseInt(txtBil1.getText());
+						int b = Integer.parseInt(txtBil2.getText());
+						int c = cbOperator.getSelectedIndex(); //memilih operator
+						if(c == 0) {
+							hasil= a+b;
+						}
+						if(c == 1) {
+							hasil = a-b;
+						}
+						if(c == 2) {
+							hasil = a*b;
+						}
+						if (c == 3) {
+							hasil = a/b;
+						}
+						if (c == 4) {
+							hasil = a%b;
+						}
+					} catch (NumberFormatException ex) {
+						pesanError("Bilangan 1 dan Bilangan 2 harus angka");
+					}
+				}
+				
+				
+				txtHasil.setText(String.valueOf(hasil));
+			}
+		});
+		btnNewButton.setBounds(166, 132, 63, 21);
+		contentPane.add(btnNewButton);
+
+	}
+}
