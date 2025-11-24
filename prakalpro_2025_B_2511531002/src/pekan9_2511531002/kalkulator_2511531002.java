66b7a66247bf23f8941e9d01c5b79eaa77def1b5
hafizhbnana <hafizhhabibullahbeniska@gmail.com>
2025-11-24 11:41:32 +0700

    pekan terakhir :(

---
+++ prakalpro_2025_B_2511531002/src/pekan9_2511531002/kalkulator_2511531002.java
@@ -0,0 +1,315 @@
+package pekan9_2511531002;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import java.awt.BorderLayout;
+import javax.swing.JButton;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class kalkulator_2511531002 {
+
+	private JFrame frame;
+	private JTextField textField;
+	
+	double first;
+	double second;
+	double result;
+	String operation;
+	String answer;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					kalkulator_2511531002 window = new kalkulator_2511531002();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public kalkulator_2511531002() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 287, 414);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		textField = new JTextField();
+		textField.setFont(new Font("Formula1 Display Bold", Font.PLAIN, 18));
+		textField.setBounds(10, 10, 253, 86);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		JButton btnBack = new JButton("\uF0E7");
+		btnBack.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String backSpace=null;
+				if(textField.getText().length()>0)
+				{
+					StringBuilder str = new StringBuilder(textField.getText());
+					str.deleteCharAt(textField.getText().length()-1);
+					backSpace=str.toString();
+					textField.setText(backSpace);
+				}
+			}
+		});
+		btnBack.setFont(new Font("Wingdings", Font.PLAIN, 18));
+		btnBack.setBounds(10, 106, 64, 53);
+		frame.getContentPane().add(btnBack);
+		
+		JButton btn7 = new JButton("7");
+		btn7.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String number=textField.getText()+btn7.getText();
+				textField.setText(number);
+			}
+		});
+		btn7.setFont(new Font("Formula1 Display Bold", Font.PLAIN, 18));
+		btn7.setBounds(10, 159, 64, 53);
+		frame.getContentPane().add(btn7);
+		
+		JButton btn4 = new JButton("4");
+		btn4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String number=textField.getText()+btn4.getText();
+				textField.setText(number);
+			}
+		});
+		btn4.setFont(new Font("Formula1 Display Bold", Font.PLAIN, 18));
+		btn4.setBounds(10, 211, 64, 53);
+		frame.getContentPane().add(btn4);
+		
+		JButton btn1 = new JButton("1");
+		btn1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String number=textField.getText()+btn1.getText();
+				textField.setText(number);
+			}
+		});
+		btn1.setFont(new Font("Formula1 Display Bold", Font.PLAIN, 18));
+		btn1.setBounds(10, 263, 64, 53);
+		frame.getContentPane().add(btn1);
+		
+		JButton btn0 = new JButton("0");
+		btn0.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String number=textField.getText()+btn0.getText();
+				textField.setText(number);
+			}
+		});
+		btn0.setFont(new Font("Formula1 Display Bold", Font.PLAIN, 18));
+		btn0.setBounds(10, 316, 64, 53);
+		frame.getContentPane().add(btn0);
+		
+		JButton btnC = new JButton("C");
+		btnC.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textField.setText(null);
+			}
+		});
+		btnC.setFont(new Font("Formula1 Display Bold", Font.PLAIN, 18));
+		btnC.setBounds(74, 106, 64, 53);
+		frame.getContentPane().add(btnC);
+		
+		JButton btn8 = new JButton("8");
+		btn8.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String number=textField.getText()+btn8.getText();
+				textField.setText(number);
+			}
+		});
+		btn8.setFont(new Font("Formula1 Display Bold", Font.PLAIN, 18));
+		btn8.setBounds(74, 159, 64, 53);
+		frame.getContentPane().add(btn8);
+		
+		JButton btn5 = new JButton("5");
+		btn5.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String number=textField.getText()+btn5.getText();
+				textField.setText(number);
+			}
+		});
+		btn5.setFont(new Font("Formula1 Display Bold", Font.PLAIN, 18));
+		btn5.setBounds(74, 211, 64, 53);
+		frame.getContentPane().add(btn5);
+		
+		JButton btn2 = new JButton("2");
+		btn2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String number=textField.getText()+btn2.getText();
+				textField.setText(number);
+			}
+		});
+		btn2.setFont(new Font("Formula1 Display Bold", Font.PLAIN, 18));
+		btn2.setBounds(74, 263, 64, 53);
+		frame.getContentPane().add(btn2);
+		
+		JButton btnDot = new JButton(".");
+		btnDot.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String number=textField.getText()+btnDot.getText();
+				textField.setText(number);
+			}
+		});
+		btnDot.setFont(new Font("Formula1 Display Bold", Font.PLAIN, 18));
+		btnDot.setBounds(74, 316, 64, 53);
+		frame.getContentPane().add(btnDot);
+		
+		JButton btn00 = new JButton("00");
+		btn00.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String number=textField.getText()+btn00.getText();
+				textField.setText(number);
+			}
+		});
+		btn00.setFont(new Font("Formula1 Display Bold", Font.PLAIN, 18));
+		btn00.setBounds(137, 106, 64, 53);
+		frame.getContentPane().add(btn00);
+		
+		JButton btnPlus = new JButton("+");
+		btnPlus.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				first=Double.parseDouble(textField.getText());
+				textField.setText("");
+				operation="+";
+			}
+		});
+		btnPlus.setFont(new Font("Formula1 Display Bold", Font.PLAIN, 18));
+		btnPlus.setBounds(202, 106, 64, 53);
+		frame.getContentPane().add(btnPlus);
+		
+		JButton btn9 = new JButton("9");
+		btn9.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String number=textField.getText()+btn9.getText();
+				textField.setText(number);
+			}
+		});
+		btn9.setFont(new Font("Formula1 Display Bold", Font.PLAIN, 18));
+		btn9.setBounds(137, 159, 64, 53);
+		frame.getContentPane().add(btn9);
+		
+		JButton btnSub = new JButton("-");
+		btnSub.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				first=Double.parseDouble(textField.getText());
+				textField.setText("");
+				operation="-";
+			}
+		});
+		btnSub.setFont(new Font("Formula1 Display Bold", Font.PLAIN, 18));
+		btnSub.setBounds(202, 159, 64, 53);
+		frame.getContentPane().add(btnSub);
+		
+		JButton btn6 = new JButton("6");
+		btn6.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String number=textField.getText()+btn6.getText();
+				textField.setText(number);
+			}
+		});
+		btn6.setFont(new Font("Formula1 Display Bold", Font.PLAIN, 18));
+		btn6.setBounds(137, 211, 64, 53);
+		frame.getContentPane().add(btn6);
+		
+		JButton btnMul = new JButton("*");
+		btnMul.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				first=Double.parseDouble(textField.getText());
+				textField.setText("");
+				operation="*";
+			}
+		});
+		btnMul.setFont(new Font("Formula1 Display Bold", Font.PLAIN, 18));
+		btnMul.setBounds(202, 211, 64, 53);
+		frame.getContentPane().add(btnMul);
+		
+		JButton btn3 = new JButton("3");
+		btn3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String number=textField.getText()+btn3.getText();
+				textField.setText(number);
+			}
+		});
+		btn3.setFont(new Font("Formula1 Display Bold", Font.PLAIN, 18));
+		btn3.setBounds(137, 263, 64, 53);
+		frame.getContentPane().add(btn3);
+		
+		JButton btnDiv = new JButton("/");
+		btnDiv.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				first=Double.parseDouble(textField.getText());
+				textField.setText("");
+				operation="/";
+			}
+		});
+		btnDiv.setFont(new Font("Formula1 Display Bold", Font.PLAIN, 18));
+		btnDiv.setBounds(202, 263, 64, 53);
+		frame.getContentPane().add(btnDiv);
+		
+		JButton btnEqual = new JButton("=");
+		btnEqual.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String answer;
+				second = Double.parseDouble(textField.getText());
+				if (operation == "+") {
+					result = first + second;
+					answer = String.format("%.2f", result);
+					textField.setText(answer);
+				}
+				else if (operation == "-") {
+					result = first - second;
+					answer = String.format("%.2f", result);
+					textField.setText(answer);
+				}
+				else if (operation == "*") {
+					result = first * second;
+					answer = String.format("%.2f", result);
+					textField.setText(answer);
+				} else if (operation == "/") {
+					result = first / second;
+					answer = String.format("%.2f", result);
+					textField.setText(answer);
+				} else if (operation == "%") {
+					result = first % second;
+					answer = String.format("%.2f", result);
+					textField.setText(answer);
+				}
+			}
+		});
+		btnEqual.setFont(new Font("Formula1 Display Bold", Font.PLAIN, 18));
+		btnEqual.setBounds(137, 316, 64, 53);
+		frame.getContentPane().add(btnEqual);
+		
+		JButton btnPercent = new JButton("%");
+		btnPercent.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				first=Double.parseDouble(textField.getText());
+				textField.setText("");
+				operation="%";
+			}
+		});
+		btnPercent.setFont(new Font("Formula1 Display Bold", Font.PLAIN, 18));
+		btnPercent.setBounds(202, 316, 64, 53);
+		frame.getContentPane().add(btnPercent);
+	}
+}
